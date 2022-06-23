42d989181ca3347724387b2d23431a58597377a0
Samuel Greco <s.greco@cybeur.eu>
2022-06-23 14:41:54 +0200

    Add express config

--- app/index.js
+++ app/index.js
@@ -10,3 +10,11 @@ const users = require("./users.json");
 // Globals Vars
 let count_users = 0;
 let online_users = [];
+
+// Express Config
+const app = express();
+const PORT = process.env.PORT || 3000;
+const server = http.createServer(app);
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static("public"))
+app.use(cookieParser());
